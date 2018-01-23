1486719ceed4669b15f31d1d6ed21a05412b20fe
Simon Larsen <simonhffh@gmail.com>
2018-01-23 22:48:54 +0100

    Removed hardcoded card effects from grammar in parser

--- cards/Card.lua
+++ cards/Card.lua
@@ -1,14 +1,14 @@
 local Card = class("cards.Card")
 
 function Card:initialize(name, token, tag, buy, scrap, active, reactive, description)
-	self.name = name
-	self.token = token
-	self.tag = tag
-	self.buy = buy
-	self.scrap = scrap
-	self.active = active
-	self.reactive = reactive
-	self.description = description
+    self.name = name
+    self.token = token
+    self.tag = tag
+    self.buy = buy
+    self.scrap = scrap
+    self.active = active
+    self.reactive = reactive
+    self.description = description
 end
 
 return Card
--- main.lua
+++ main.lua
@@ -6,11 +6,11 @@ local lovebird = require("lovebird")
 local Parser = require("cards.Parser")
 
 function prox.load()
-	prox.window.set(640, 360, true, 2, false, "scale")
-	local parser = Parser()
-	cards = parser:readCards("data/cards.csv")
+    prox.window.set(640, 360, true, 2, false, "scale")
+    local parser = Parser()
+    --cards = parser:readCards("data/cards.csv")
 end
 
 function prox.update(dt)
-	lovebird.update()
+    lovebird.update()
 end
