1c2b903256bc1a67a7ff9658ff3bc5625afa5b10
Simon Larsen <simonhffh@gmail.com>
2018-01-28 15:34:37 +0100

    Added evaluation fun. to Condition

--- cards/Condition.lua
+++ cards/Condition.lua
@@ -1,9 +1,23 @@
 local Condition = class("cards.Condition")
 
+local operators = {
+    [">"]  = function(a, b) return a > b end,
+    ["<"]  = function(a, b) return a < b end,
+    [">="] = function(a, b) return a >= b end,
+    ["<="] = function(a, b) return a <= b end,
+    ["!="] = function(a, b) return a ~= b end,
+    ["=="] = function(a, b) return a == b end
+}
+
 function Condition:initialize(variable, op, value)
     self.variable = variable
-    self.op = op
+    self.op = operators[op]
     self.value = value
+end
+
+function Condition:eval(vars)
+    local var = vars[self.variable]
+    return self.op(var, self.value)
 end
 
 return Condition
