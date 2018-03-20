5a5ac6d6688b7e5daaa409ea4bd7affe14306cb1
Simon Larsen <simonhffh@gmail.com>
2018-03-20 10:59:35 +0100

    Added documentation to Pile class

--- battle/Pile.lua
+++ battle/Pile.lua
@@ -1,5 +1,9 @@
+--- Card pile abstract datastructure
+-- @classmod battle.Pile
 local Pile = class("battle.Pile")
 
+--- Constructor.
+-- @param cards (optional) Table of @{cards.Card} to add to pile
 function Pile:initialize(cards)
     if cards then
         self._cards = prox.table.copy(cards)
@@ -8,16 +12,22 @@ function Pile:initialize(cards)
     end
 end
 
+--- Add card to pile.
+-- @param card @{cards.Card} instance of card to add to top of pile.
 function Pile:addCard(card)
     table.insert(self._cards, 1, card)
 end
 
+--- Add stack of cards to pile.
+-- @param cards Table of @{cards.Card} instances to add to top of pile. First card in stack will be added on top.
 function Pile:addCards(cards)
-    for i,v in ipairs(cards) do
-        self:addCard(v)
+    for i=#cards,1,-1 do
+        self:addCard(cards[i])
     end
 end
 
+--- Draw a card from pile.
+-- @param i (optional) Position in pile to draw. If not given, top card will be drawn.
 function Pile:draw(i)
     i = i or 1
     local c = self._cards[i]
@@ -25,18 +35,22 @@ function Pile:draw(i)
     return c
 end
 
+--- Shuffle pile.
 function Pile:shuffle()
     prox.table.shuffle(self._cards)
 end
 
+--- Empty pile.
 function Pile:clear()
     self._cards = {}
 end
 
+--- Get number of cards in pile.
 function Pile:size()
     return #self._cards
 end
 
+--- Get table of cards in pile.
 function Pile:getCards()
     return self._cards
 end
