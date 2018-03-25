054ecfe9dce39a9eb92d9689bbc7d970513dafd9
Simon Larsen <simonhffh@gmail.com>
2018-03-25 17:29:20 +0200

    Use missing card placeholder in AssetManager

--- core/AssetManager.lua
+++ core/AssetManager.lua
@@ -1,13 +1,13 @@
 local AssetManager = class("core.AssetManager")
 
-local MISSING_CARD = "data/images/cards/_missing_.png"
+local MISSING_CARD_PATH = "data/images/cards/_missing_.png"
 
 function AssetManager:getCard(id)
-    local img = prox.resources.getImage("data/images/cards/" .. id .. ".png", false)
-    if img == nil then
-        img = prox.resources.getImage(MISSING_CARD)
+    local path = "data/images/cards/" .. id .. ".png"
+    if not prox.resources.exists(path) then
+        path = MISSING_CARD_PATH
     end
-    return img
+    return prox.resources.getImage(path)
 end
 
 return AssetManager
